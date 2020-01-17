ad94275bca44808e00986c38b2036cedae09b426
Diego Chaves <dfchaves@gmail.com>
2020-01-17 10:01:25 -0300

    Adição de Pesquisa

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,26 +1,35 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
 
 module.exports = {
+  async index (request, response) {
+    const devs = Dev.find()
+    return response.json(devs)
+  },
   async store (request, response) {
     const {github_username, techs, latitude, longitude} = request.body
-    const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-    const {name = login, avatar_url, bio} = githubApiResponse.data
-    const techsArray = techs.split(',').map(tech => tech.trim())
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
+    let dev = await Dev.findOne({github_username})
+
+    if (!dev) {
+      const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+      const {name = login, avatar_url, bio} = githubApiResponse.data
+      const techsArray = parseStringAsArray(techs)
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      }
+      const devData = {
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      }
+      dev = await Dev.create(devData)
     }
-    const devData = {
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    }
-    
-    const dev = await Dev.create(devData)
-    
+
     return response.json(dev)
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 const { Router } = require('express')
 const routes = Router()
 const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
 routes.get('/', (request, response) => {
   console.log(request.query)
   return response.json({message: 'Hello World!'})
 })
 
+routes.get('/dev', DevController.index)
 routes.post('/dev', DevController.store)
+
+routes.get('/search', SaerchController.index)
 
 module.exports = routes
